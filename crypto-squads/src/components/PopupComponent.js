a51110772a0eb189292dacf8d4dd09879ef6df46
BelhajMokhlis <belhajmokhlis@gmail.com>
2024-04-20 00:52:31 +0100

    filtre + popup detail

---
+++ crypto-squads/src/components/PopupComponent.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import useApiAxios from '../config/axios';
+
+const PopupComponent = ({ id, onClose }) => {
+    const [data, setData] = useState(null);
+    
+    useEffect(() => {
+        fetchAsset();
+    }, []);
+
+    const fetchAsset = () => {
+        useApiAxios.get(`/assets/${id}`)
+            .then(response => {
+                setData(response.data.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    };
+
+    return (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
+            <div className="bg-white p-6 rounded-lg shadow-lg max-w-md">
+                <h2 className="text-xl font-semibold mb-4">Asset Details</h2>
+                {data ? (
+                    <div>
+                        <div className="mb-4">
+                            <h3 className="text-lg font-semibold mb-2">Basic Information</h3>
+                            <p><strong>Name:</strong> {data.name}</p>
+                            <p><strong>Symbol:</strong> {data.symbol}</p>
+                            <p><strong>Rank:</strong> {data.rank}</p>
+                        </div>
+                        <div className="mb-4">
+                            <h3 className="text-lg font-semibold mb-2">Market Information</h3>
+                            <p><strong>Price (USD):</strong> {data.priceUsd}</p>
+                            <p><strong>Market Cap (USD):</strong> {data.marketCapUsd}</p>
+                            <p><strong>Volume (USD 24Hr):</strong> {data.volumeUsd24Hr}</p>
+                            <p><strong>Change (24Hr):</strong> {data.changePercent24Hr}%</p>
+                            <p><strong>VWAP (24Hr):</strong> {data.vwap24Hr}</p>
+                        </div>
+                        <div>
+                            <a href={data.explorer} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Explore more info</a>
+                        </div>
+                    </div>
+                ) : (
+                    <p>Loading...</p>
+                )}
+                <button
+                    className="mt-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md mr-2"
+                    onClick={onClose}
+                >
+                    Close
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default PopupComponent;
--- crypto-squads/src/pages/Assets.js
+++ crypto-squads/src/pages/Assets.js
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import DataTable from 'react-data-table-component';
+import useApiAxios from '../config/axios';
+import PopupComponent from '../components/PopupComponent';
 
 function Assets() {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedRowId, setSelectedRowId] = useState(null); // State to hold the ID of the selected row
+
+  useEffect(() => {
+      fetchAssetes();
+  }, []);
+
+  const fetchAssetes = () => {
+      useApiAxios.get('/assets')
+          .then(response => {
+              setData(response.data.data);
+          })
+          .catch(error => {
+              console.error('Error fetching data:', error);
+          });
+  };
+
+  const handleRowClick = (row) => {
+    setSelectedRowId(row.id); // Set the ID of the clicked row
+  };
+
+  const handleClosePopup = () => {
+    setSelectedRowId(null); // Close the popup by resetting the selectedRowId state
+  };
+  const filteredData =  data.filter((item) =>
+        item.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+
+  const columns = [
+    {
+        name: 'ID',
+        selector: row => row.id,
+        sortable: true,
+    },
+    {
+        name: 'Rank',
+        selector: row => row.rank,
+        sortable: true,
+    },
+    {
+        name: 'Symbol',
+        selector: row => row.symbol,
+        sortable: true,
+    },
+    {
+        name: 'Name',
+        selector: row => row.name,
+        sortable: true,
+    },
+    {
+        name: 'Price USD',
+        selector: row => row.priceUsd,
+        sortable: true,
+    },
+  ];
+
   return (
-    <div>Assets</div>
-  )
+      <div className="max-w-4xl mx-auto px-4 py-8">
+          <div className='flex justify-between py-1'>
+              <h2 className="text-2xl mb-4">Assets</h2>
+          </div>
+
+          <div className="mb-4">
+              <input
+                  type="text"
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                  onChange={e => setSearchTerm(e.target.value)}
+                  placeholder="Search..."
+              />
+          </div>
+
+          <DataTable
+              columns={columns}
+              data={filteredData}
+              pagination
+              highlightOnHover
+              striped
+              onRowClicked={handleRowClick} // Attach click event handler
+          />
+
+          {selectedRowId && <PopupComponent id={selectedRowId} onClose={handleClosePopup} />} {/* Render PopupComponent if row ID is set */}
+      </div>
+  );
 }
 
-export default Assets
+export default Assets;
